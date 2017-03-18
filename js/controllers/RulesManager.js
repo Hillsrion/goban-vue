1dc2f0afcdd1d78615f8ada02bbb581c77e40e17
Ismaël Sebbane <ismael.sebbane@gmail.com>
2017-03-18 22:43:56 +0100

    Created method to make unfillable a slot if the 4 adjacents are taken by the same person.

--- js/controllers/RulesManager.js
+++ js/controllers/RulesManager.js
@@ -11,28 +11,32 @@ class RulesManager {
             koList: []
         };
     }
+
     eval(goban) {
-        if(goban) {
+        if (goban) {
             this.currentGoban = goban;
             let slot;
-            for(let key in this.currentGoban) {
+            for (let key in this.currentGoban) {
                 slot = this.currentGoban[key];
-                if(this._isConnectedSlot(slot)) {
+                if (this._isConnectedSlot(slot)) {
                     // This is logged for all slots
                     //console.log("slot is connected");
-                    if(slot.lastUsed) {
-                        console.log("slot is connected")
+                    if (slot.lastUsed) {
+                        // console.log("slot is connected")
                     }
                 } else {
                     // This is logged for all slots
                     //console.log("slot is connected");
-                    if(slot.lastUsed) {
-                        console.log("slot is not connected")
+                    if (slot.lastUsed) {
+                        // console.log("slot is not connected")
                     }
-                    if(slot.isFilled && this._isAtari(slot)) {
+                    if (slot.isFilled && this._isAtari(slot)) {
                         this._putAtari(slot);
-                    } else if(slot.isFilled && this._isDeadKilledBy(slot)) {
-                        this._killSlot(slot);
+                    } else if (slot.isFilled && this._isDeadKilledBy(slot)) {
+                        this._killSingleSlot(slot);
+                    }
+                    if (!slot.isFilled && this._isUnfillableByOpponent(slot)) {
+                        slot.isFillableBy = this.lastReference;
                     }
                 }
             }
@@ -41,20 +45,28 @@ class RulesManager {
             console.warn("goban given in param is null");
         }
     }
-    getAtariList() {
+    _getConnectedArea() {
+
     }
     _putAtari(slot) {
         slot.isAtari = true;
         console.log(`slot position ${slot.x},${slot.y} is in Atari`);
         this.dataTurn.atariList.push(slot);
     }
-    _killSlot(slot) {
+
+    _killSingleSlot(slot) {
+        /**
+         *
+         * Here we're gonna reset the slot state so it can be refilled if possible.
+         */
         slot.isFilled = false;
         slot.isFillableBy = this.lastKilledBy;
         slot.isAtari = false;
+        slot.belongsTo = "";
         console.log(`slot position ${slot.x},${slot.y} is dead`);
         this.dataTurn.deathList.push(slot);
     }
+
     _getAdjacentSlots(slot) {
         /**
          *  In the case we're dealing slots close the bounds of the goban,
@@ -63,6 +75,7 @@ class RulesManager {
          */
         const x = parseInt(slot.x);
         const y = parseInt(slot.y);
+        let goban = this.currentGoban;
         /**
          * I parse x and y as int, and make the calculations aside
          * because it can be interpreted as a concatenation
@@ -70,22 +83,62 @@ class RulesManager {
          * @type {{x1: {slotModel}, x2: {slotModel}, y1: {slotModel}, y2: {slotModel}}}
          */
         const adjacentSlots = {
-            x1: this.currentGoban[(x-1)+","+y],
-            x2: this.currentGoban[(x+1)+","+y],
-            y1: this.currentGoban[x+","+(y-1)],
-            y2: this.currentGoban[x+","+(y+1)]
+            x1: goban[(x - 1) + "," + y],
+            x2: goban[(x + 1) + "," + y],
+            y1: goban[x + "," + (y - 1)],
+            y2: goban[x + "," + (y + 1)]
         };
         return adjacentSlots;
     }
+
     _isConnectedSlot(slot) {
         const adjacentSlots = this._getAdjacentSlots(slot);
         // console.log(adjacentSlots);
         let sibling;
-        for(let key in adjacentSlots) {
+        for (let key in adjacentSlots) {
             sibling = adjacentSlots[key];
-            if(sibling && sibling.isFilled && sibling.belongsTo==slot.belongsTo) {
+            if (sibling && sibling.isFilled && sibling.belongsTo == slot.belongsTo) {
                 return true
             }
+        }
+    }
+
+    /**
+     * Returns true if the unfilled slot given in parameter mustn't be filled by the opponent
+     * @param slot
+     * @returns {boolean}
+     * @private
+     */
+    _isUnfillableByOpponent(slot) {
+        const adjacentSlots = this._getAdjacentSlots(slot);
+        // console.log(adjacentSlots);
+        let sibling;
+        let owners = {
+            reference: "",
+            colors: []
+        };
+        let result;
+        for (let key in adjacentSlots) {
+            sibling = adjacentSlots[key];
+            if (sibling && sibling.isFilled) {
+                owners.colors.push(sibling.belongsTo);
+                if(!owners.reference) {
+                    owners.reference = sibling.belongsTo;
+                    this.lastReference = sibling.belongsTo;
+                }
+            } else if(!sibling) {
+                owners.colors.push("");
+            }
+        }
+        // if reference property is a empty chain, then none of the adjacent slots belong to someone.
+        if(owners.reference=="") {
+            return false
+        } else {
+            // If the 4 adjacent slots has the same owner or are empty for some, then the slot if unfillable for the opponent.
+            const predicate = owners.colors.every((slot) => {
+                return slot==owners.reference || slot==""
+            });
+            return predicate && owners.colors.length==4;
         }
     }
 
@@ -104,17 +157,22 @@ class RulesManager {
          * We're gonna test if adjacents slots belong to the opponent.
          * If 3 adjacents slots do, then the slot is in atari.
          */
-        for(let key in adjacentSlots) {
+        for (let key in adjacentSlots) {
             sibling = adjacentSlots[key];
-            if(sibling && sibling.isFilled && sibling.belongsTo!==slot.belongsTo) {
+            if (sibling && sibling.isFilled && sibling.belongsTo !== slot.belongsTo) {
+                i++;
+            } else if(!sibling) {
+                // If sibling doesn't exist, it's that we're on a border
                 i++;
             }
         }
-        return i==3
+        return i == 3
     }
+
     _isKo(position) {
         return true;
     }
+
     /**
      * Tests if the stone is dead or not and returns the opponent color.
      * @param slot
@@ -132,25 +190,23 @@ class RulesManager {
          * We're gonna test if adjacents slots belong to the opponent.
          * If 4 adjacents slots do, then the slot is dead.
          */
-        for(let key in adjacentSlots) {
+        for (let key in adjacentSlots) {
             sibling = adjacentSlots[key];
-            if(sibling && sibling.isFilled && sibling.belongsTo!==slot.belongsTo) {
+            if (sibling && sibling.isFilled && sibling.belongsTo !== slot.belongsTo) {
+                i++;
+            } else if(!sibling) {
+                // If sibling doesn't exist, it's that we're on a border
                 i++;
             }
-            if(i==4) {
+            if (i == 4) {
                 killedBy = sibling.belongsTo;
                 this.lastKilledBy = killedBy
             }
         }
         return killedBy;
     }
-    getConnectedArea() {
-
-    }
-    getTakenStoneList() {
-    }
-
 }
+
 
 
 export default new RulesManager()
